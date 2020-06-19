24a3d480b7d89842fa5b63fb0560a9cacabbce46
Devansh Patel <devanshpatel9300@gmail.com>
2020-06-19 16:06:52 -0400

    Updates

---
+++ precache-manifest.ccc3eab3f55514f8d70942e6adaa36e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba9e0ef38731c3b6dc256d0e15fd1a8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f2572420ef65969ed361",
+    "url": "/portfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "508a9d370d058193b480",
+    "url": "/portfolio/static/css/main.75e4af36.chunk.css"
+  },
+  {
+    "revision": "f2572420ef65969ed361",
+    "url": "/portfolio/static/js/2.6e8f8998.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio/static/js/2.6e8f8998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "508a9d370d058193b480",
+    "url": "/portfolio/static/js/main.52536f02.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  }
+]);
